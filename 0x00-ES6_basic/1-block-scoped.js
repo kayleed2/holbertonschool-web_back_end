e1ed82384a2d4ff9fa2452e57ec2c79d272f2f8d
Kaylee Dao <kayleed2@gmail.com>
2022-05-02 12:32:10 -0500

    indentation

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // Multiple Functions
 
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-  
+export default function taskBlock (trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
-  
+
   return [task, task2];
 }

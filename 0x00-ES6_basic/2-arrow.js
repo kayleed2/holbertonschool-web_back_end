0dfa960f929ba81d96d7f12cd0f7e11342c21ffd
Kaylee Dao <kayleed2@gmail.com>
2022-05-02 12:29:22 -0500

    Arrow, anonymous

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Multiple Functions
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}

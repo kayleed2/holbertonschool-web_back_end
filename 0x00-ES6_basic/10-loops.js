4b507559d2a4ad9a2387c75810b0bd91c920c903
Kaylee Dao <kayleed2@gmail.com>
2022-05-02 18:05:09 -0500

    Changing only one list

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const tmp = array;
+  const tmp = [];
   for (const idx of array) {
-    const value = tmp[idx];
+    const value = array[idx];
     tmp[idx] = appendString + value;
   }
 

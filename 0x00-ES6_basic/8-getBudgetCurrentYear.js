b1719ae0032a959838098466ad2e10cd2a03d82d
Kaylee Dao <kayleed2@gmail.com>
2022-05-02 17:16:37 -0500

    Setting object immediately

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,15 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {
-    income,
-    gdp,
-    capita,
-  };
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
+  const budget = new Object();
+  budget.`income-${getCurrentYear()}` = income;
+  budget.`gdp-${getCurrentYear()}` = gdp;
+  budget.`capita-${getCurrentYear()}` = capita;
 
   return budget;
 }

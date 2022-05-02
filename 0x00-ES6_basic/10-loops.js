26267e0a51523bd298a2dd62255c53e6d11df08d
Kaylee Dao <kayleed2@gmail.com>
2022-05-02 17:56:57 -0500

    New var

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const tmp = array;
-  for (const idx of array) {
+  for (const idx of tmp) {
     const value = tmp[idx];
     tmp[idx] = appendString + value;
   }

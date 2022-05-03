2b5cc2b93bd985a1c94a6799665d0f36342972b7
Kaylee Dao <kayleed2@gmail.com>
2022-05-03 14:17:50 -0500

    Trying await

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  Promise.allSettled([user, photo]).then((response) => response);
+  async function f1() {
+    const x = await Promise.allSettled([user, photo]).then((response) => response);
+    return x;
+  }
+  f1();
 }

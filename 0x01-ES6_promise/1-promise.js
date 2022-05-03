be84915a993d5fb87aaaf3d19381b61cd0e837ed
Kaylee Dao <kayleed2@gmail.com>
2022-05-03 12:29:24 -0500

    THEN

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   if (success === true) {
-    const dic = { status: 200, body: 'Sucess' };
+    const dic = { status: 200, body: 'Success' };
     return Promise.resolve(dic);
   }
   return Promise.reject(new Error('The fake API is not working currently'));
---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(
+    () => {
+      const dic = { status: 200, body: 'Success' };
+      console.log('Got a response from the API');
+      return Promise.resolve(dic);
+    },
+    () => new Error(),
+  );
+}

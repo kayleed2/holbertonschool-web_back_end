8d66c5cb6f69b5f329713ee637664c076e2be9d0
Kaylee Dao <kayleed2@gmail.com>
2022-05-04 10:58:52 -0500

    Push e message, not object

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  const result = mathFunction();
-  if (result instanceof Error) {
-    queue.push(result.message);
+  try {
+    mathFunction();
+  } catch (e) {
+    queue.push(`${e.name}: ${e.message}`);
     queue.push('Guardrail was processed');
-  } else {
-    queue.push(result);
-    queue.push('Guardrail was processed');
+    return queue;
   }
+  queue.push(mathFunction());
+  queue.push('Guardrail was processed');
   return queue;
 }

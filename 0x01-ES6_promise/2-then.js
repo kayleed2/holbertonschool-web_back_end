12e0cc206528d405278111d561bacaadcb781ac2
Kaylee Dao <kayleed2@gmail.com>
2022-05-05 08:58:51 -0500

    Lowercase success

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    () => {
-      const dic = { status: 200, body: 'success' };
-      console.log('Got a response from the API');
-      return Promise.resolve(dic);
-    },
-    () => new Error(),
-  );
+  return promise
+    .then(
+      () => ({ status: 200, body: 'success' }),
+    )
+    .catch(
+      () => new Error(),
+    )
+    .finally(() => console.log('Got a response from the API'));
 }

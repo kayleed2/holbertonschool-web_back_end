fd08231951a843ee8b1e91407ab0501ecf1836d7
Kaylee Dao <kayleed2@gmail.com>
2022-05-05 09:12:24 -0500

    return two await

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignUp(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-  async function f1() {
-    const x = await Promise.allSettled([user, photo]).then((response) => response);
-    return x;
-  }
-  const result = f1();
-  return result;
+export default async function handleProfileSignUp(firstName, lastName, fileName) {
+  return [
+    await signUpUser(firstName, lastName)
+      .then((response) => response),
+    await uploadPhoto(fileName)
+      .catch((response) => response),
+  ];
 }

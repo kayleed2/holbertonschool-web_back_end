d45a899f41905870a96d881a0358d70b1a5f657f
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 10:29:05 -0500

    Get, set, method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+/*
+eslint no-underscore-dangle: ["error", { "allowAfterThis": true }]
+*/
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}

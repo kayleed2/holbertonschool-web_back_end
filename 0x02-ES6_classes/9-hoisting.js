929e98c061c7e4fbb8cff1d97bbb86c3a0a4894d
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 14:14:24 -0500

    Retry

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -32,10 +32,6 @@ export class StudentHolberton {
   get holbertonClass() {
     return this._holbertonClass;
   }
-
-  get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-  }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');

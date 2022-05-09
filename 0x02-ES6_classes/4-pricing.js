8126f233325b2ccc9bf995ca3f7a8e20b408ee4a
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 11:52:00 -0500

    typeof newAmount

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+/*
+eslint no-underscore-dangle: ["error", { "allowAfterThis": true }]
+*/
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw TypeError('Amount must be a number');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw TypeError('Currency must be a currency');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+  }
+}

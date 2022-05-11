4f466a723ee240e7733a488af82440076754b663
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 12:58:13 -0500

    Check just starting

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   const strings = [];
   let app;
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   for (const el of set) {
-    if (el.startsWith(startString)) {
+    if (el.startsWith(startString) && typeof el === 'string') {
       app = el.replace(startString, '');
       strings.push(app);
     }

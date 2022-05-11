0f948fd5c06ccaaed4f9e9488b92ace554a31a28
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 08:12:39 -0500

    Set individual objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
+  const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [studentOne, studentTwo, studentThree];
+}

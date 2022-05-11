4b7a43a86bd30257ad4609423c65abb227ada5a5
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 12:00:02 -0500

    Send error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buff = new ArrayBuffer(length);
   const view = new DataView(buff, 0, length);
-  view.setInt8(position, value);
+  try {
+    view.setInt8(position, value);
+  } catch (error) {
+    console.log('Position outside of range');
+  }
   return view;
 }

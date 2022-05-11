97e64328f0edf6d167d1976cfb048f425861b749
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 12:06:01 -0500

    try the whole block

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position > length || position < 0) {
-    throw Error('Position outside of range');
+  try {
+    const buff = new ArrayBuffer(length);
+    const view = new DataView(buff, 0, length);
+    view.setInt8(position, value);
+    return view;
+  } catch (error) {
+    throw new Error('Position outside of range');
   }
-  const buff = new ArrayBuffer(length);
-  const view = new DataView(buff, 0, length);
-  view.setInt8(position, value);
-  return view;
 }

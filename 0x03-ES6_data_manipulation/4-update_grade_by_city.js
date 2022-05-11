efa68268a07bf0b13eb8788c9d32b7236cc231de
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 11:41:30 -0500

    Have to change var name to grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList.filter((obj) => obj.location === city)
-    .map((st) => {
-      let num = newGrades.filter((gr) => gr.studentId === st.id);
-      if (num.length === 0) {
-        num = 'N/A';
-      } else {
-        num = num[0].grade;
-      }
-      return { ...st, num };
-    });
+  const cityStudents = studentList.filter((obj) => obj.location === city);
+  return cityStudents.map((st) => {
+    let grade = newGrades.filter((grade) => grade.studentId === st.id);
+    if (grade.length === 0) {
+      grade = 'N/A';
+    } else {
+      grade = grade[0].grade;
+    }
+    return { ...st, grade };
+  });
 }

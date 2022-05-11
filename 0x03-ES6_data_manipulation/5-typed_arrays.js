78fabd2dde97f9fe6ea503aecb17a80d9f1f3e17
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 12:08:02 -0500

    Correcting error message

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,6 +5,6 @@ export default function createInt8TypedArray(length, position, value) {
     view.setInt8(position, value);
     return view;
   } catch (error) {
-    throw new Error('Position outside of range');
+    throw new Error('Position outside range');
   }
 }

9b93c84adc2f7d80c490ccb08c2bab0c86df378f
Kaylee Dao <kayleed2@gmail.com>
2022-05-11 11:57:39 -0500

    Set the position after

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,6 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buff = new ArrayBuffer(length);
+  const view = new DataView(buff, 0, length);
+  view.setInt8(position, value);
+  return view;
+}

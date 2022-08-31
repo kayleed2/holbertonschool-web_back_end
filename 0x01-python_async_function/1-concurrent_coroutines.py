136096e6c1e4a0139cc18a89c947f4740c14ca8f
Kaylee Dao <kayleed2@gmail.com>
2022-08-30 20:16:25 -0500

    waiting for individual delay up until n

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async"""
+
+
+wait_random = __import__('0-basic_async_syntax.py').wait_random
+from typing import List
+import asyncio
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """waits for a random delay between 0 and max_delay"""
+    execs = []
+    complete = []
+
+    for i in range(n):
+        exec = asyncio.create_task(wait_random(max_delay))
+        execs.append(exec)
+
+    for exec in asyncio.as_completed(execs):
+        result: float = await exec
+        complete.append(result)
+
+    return complete

5d03a3816a6162d0f718662fe373286deeea0c8a
Kaylee Dao <kayleed2@gmail.com>
2022-08-31 09:21:22 -0500

    Itertating using async for

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Write a coroutine called async_generator that takes no arguments."""
+
+
+import asyncio
+from typing import List
+import random
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """The coroutine will collect
+    10 random numbers using an async
+    comprehensing over async_generator,
+    then return the 10 random numbers."""
+    thing = [i async for i in async_generator()]
+    return thing

2cdae69234a113f38daf067c23b0d889c6bfd021
Kaylee Dao <kayleed2@gmail.com>
2022-08-31 14:05:05 -0500

    Using gather to run multiple coroutines

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather"""
+
+
+import asyncio
+from typing import List
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """coroutine that will execute async_comprehension four times """
+    start = time.time()
+    
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+
+    end = time.time()
+    
+    return (end - start)

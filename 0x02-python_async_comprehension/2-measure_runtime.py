e9fb1c2c15a164ffc9d89754c7abf0c3591d2ab3
Kaylee Dao <kayleed2@gmail.com>
2022-08-31 14:09:38 -0500

    pycodestyle

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,17 +4,17 @@ four times in parallel using asyncio.gather"""
 
 
 import asyncio
-from typing import List
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """coroutine that will execute async_comprehension four times """
     start = time.time()
-    
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
-
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension())
     end = time.time()
-    
     return (end - start)

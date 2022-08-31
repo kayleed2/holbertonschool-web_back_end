fd2bc02c7f2f8d5b94a2c177523dc39ce53e7c83
Kaylee Dao <kayleed2@gmail.com>
2022-08-31 09:21:45 -0500

    Itertating using async for

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,7 @@
 """Write a coroutine called async_generator that takes no arguments."""
 
 
-import asyncio
 from typing import List
-import random
 async_generator = __import__('0-async_generator').async_generator
 
 

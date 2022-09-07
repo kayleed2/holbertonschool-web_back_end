4766c459d09005d373b29ae472c87b9b1601dc41
Kaylee Dao <kayleed2@gmail.com>
2022-09-07 10:28:18 -0500

    Don't replace C

---
+++ 0x00-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+class LIFOCache that inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    class LIFOCache that
+    inherits from BaseCaching and
+    is a caching system:
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        self.dict_list = list(self.cache_data)
+        if (key and item):
+            if key in self.cache_data:
+                self.cache_data.pop(key)
+            self.cache_data[key] = item
+
+            if ((len(self.cache_data) > super().MAX_ITEMS)):
+                el = self.dict_list[super().MAX_ITEMS - 1]
+                del self.cache_data[el]
+                print("DISCARD:", el[0])
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if (key is None or key not in self.cache_data):
+            return None
+        return self.cache_data[key]

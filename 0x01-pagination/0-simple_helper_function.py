ffe432f225bbf17d290976b2910c1f7cfc07bb8b
Kaylee Dao <kayleed2@gmail.com>
2022-09-08 09:27:31 -0500

    Checking if page == 0

--- 0x01-pagination/0-simple_helper_function.py
+++ 0x01-pagination/0-simple_helper_function.py
@@ -5,9 +5,18 @@ that takes two integer arguments
 """
 
 
+from turtle import st
+
+
 def index_range(page, page_size):
     """return a tuple of size two
     containing a start index and an end index"""
-    end = page_size * page
-    start = (page - 1) * page_size
-    return (start, end)
+    if page == 0 and page_size:
+        end = page_size
+        start = 0
+        return (start, end)
+    
+    if (page and page_size):
+        end = page_size * page
+        start = (page - 1) * page_size
+        return (start, end)

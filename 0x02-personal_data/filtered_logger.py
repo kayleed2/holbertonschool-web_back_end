d841ca8169abb50e3dd87d5814556fcaaf8e2526
Kaylee Dao <kayleed2@gmail.com>
2022-09-13 09:18:02 -0500

    Initial commit

---
+++ 0x02-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function called filter_datum
+that returns the log message
+obfuscated
+"""
+
+from typing import List
+
+
+def filter_datum (fields: List, redaction: str, message: str, separator: str) -> str:
+    """a function called filter_datum
+    that returns the log message obfuscated:"""
+    

e5bd7f7217b157f60ccce42955767fc02751d53a
Kaylee Dao <kayleed2@gmail.com>
2022-09-14 12:02:00 -0500

    pycodestyle

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -11,6 +11,7 @@ def hash_password(password: str) -> bytes:
     """Takes password as string and hashes it"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks is password is valid or not"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

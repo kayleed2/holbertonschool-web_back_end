b2361b8e40c4c3ba8f80f7264bdecf44f4651ede
Kaylee Dao <kayleed2@gmail.com>
2022-09-14 10:49:19 -0500

    annotations

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ import re
 from typing import List
 
 
-def filter_datum (fields: List, redaction: str, message: str, separator: str) -> str:
+def filter_datum (fields: List[str], redaction: str, message: str, separator: str) -> str:
     """a function called filter_datum
     that returns the log message obfuscated:"""
     for el in fields:

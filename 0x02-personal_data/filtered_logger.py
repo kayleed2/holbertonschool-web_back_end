42524693e4702c521411a2e338a7a113ec1f0ae4
Kaylee Dao <kayleed2@gmail.com>
2022-09-14 11:19:12 -0500

    Redacting

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -7,6 +7,26 @@ obfuscated
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initiate class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formatting the record"""
+        ms = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, ms, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,

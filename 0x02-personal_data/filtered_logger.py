e59f5cbedd1a2384bdad3495580be0617cbedc12
Kaylee Dao <kayleed2@gmail.com>
2022-09-14 10:48:15 -0500

    task 0

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -5,10 +5,14 @@ that returns the log message
 obfuscated
 """
 
+import re
 from typing import List
 
 
 def filter_datum (fields: List, redaction: str, message: str, separator: str) -> str:
     """a function called filter_datum
     that returns the log message obfuscated:"""
-    
+    for el in fields:
+        pattern = f"(?<={el}=).*?(?={separator})"
+        string = re.sub(pattern, redaction, message)
+    return message

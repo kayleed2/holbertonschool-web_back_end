c52812651ccf20eac88bd306bb16021553333dfb
Kaylee Dao <kayleed2@gmail.com>
2022-09-14 12:00:47 -0500

    is_valid

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -10,3 +10,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Takes password as string and hashes it"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is password is valid or not"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

0d309553cb483f6c72f62647e3d547f165410546
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 08:07:32 -0500

    Creating auth class

---
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module to create auth class"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class to define authorization"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns boolean of if user is auth"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """This will get flask request object"""
+        return request
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """This will get flask request object"""
+        return request

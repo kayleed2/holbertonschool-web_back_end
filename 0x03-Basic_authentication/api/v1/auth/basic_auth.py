70ed02faa94bb6d3b9ef96caa2ef7f9947f0b425
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 11:17:46 -0500

    pycodestyle

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import re
 from base64 import b64decode
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -56,3 +57,21 @@ class BasicAuth(Auth):
             return decoded_base64_authorization_header.split(':')
         else:
             return None, None
+
+    def user_object_from_credentials(self, 
+                                     user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Returns the User instance based on his email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        
+        try:
+            search = User.search({'email': user_email})
+
+            for user in search:
+                if user.is_valid_password(user_pwd):
+                    return user
+
+        except Exception:
+            return None

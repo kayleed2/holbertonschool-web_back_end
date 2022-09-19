97f5f51ee5328bb388fa42cbb0cf3c2ee8818020
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 11:01:58 -0500

    Adding decode

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import re
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -27,3 +28,17 @@ class BasicAuth(Auth):
             if len(new) != 2:
                 return None
             return new[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str):
+        """Returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+
+        except Exception:
+            return None

4e380134e882ffc094bf29ac67cb16a7752b876b
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 08:08:20 -0500

    pycodestyle

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -12,11 +12,11 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns boolean of if user is auth"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """This will get flask request object"""
         return request
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """This will get flask request object"""
         return request

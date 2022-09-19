baa9f58ffb96fb6d44c0c9f5711410c306fbaeb9
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 10:53:39 -0500

    Using regex to find match

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,25 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """class to define authorization"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Returns the Base64 part of the Authorization
+        header for a Basic Authentication"""
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        string = re.findall("Basic", authorization_header)
+        if string == []:
+            return None
+        else:
+            new = re.split("\s", authorization_header, 1)
+            if len(new) != 2:
+                return None
+            return new[1]

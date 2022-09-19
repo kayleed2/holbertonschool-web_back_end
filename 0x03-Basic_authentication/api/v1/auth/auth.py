88236748020eff0a9eb1b29fddcef7d3d150518e
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 08:18:03 -0500

    Most

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,13 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns boolean of if user is auth"""
-        if path is None or path not in excluded_paths:
+        if path is None:
             return True
         
-        if excluded_paths is None or excluded_paths == "/":
+        if path not in excluded_paths:
+            return True
+
+        if excluded_paths is None or excluded_paths == "[]":
             return True
 
         if path in excluded_paths:

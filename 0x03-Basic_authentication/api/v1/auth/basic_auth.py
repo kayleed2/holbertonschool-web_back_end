cb66c2395271480d2cc85f2247f5bb8bae4006b2
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 11:10:50 -0500

    pycodestyle

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,15 @@ class BasicAuth(Auth):
 
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str):
+        """Returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        
+        if ':' in decoded_base64_authorization_header:
+            return decoded_base64_authorization_header.split(':')
+

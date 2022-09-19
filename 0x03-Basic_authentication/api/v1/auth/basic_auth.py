1d939161f607adc8f81472a2d92a28af8185f854
Kaylee Dao <kayleed2@gmail.com>
2022-09-19 11:03:38 -0500

    pycodestyle

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,7 +29,8 @@ class BasicAuth(Auth):
                 return None
             return new[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str):
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str):
         """Returns the decoded value of a Base64 string"""
         if base64_authorization_header is None:
             return None

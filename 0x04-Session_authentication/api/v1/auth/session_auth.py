af1c561fb5e43336c2a26570e6ee946c24a84378
Kaylee Dao <kayleed2@gmail.com>
2022-09-21 09:41:34 -0500

    Creating session

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -2,13 +2,19 @@
 """Module to create auth class"""
 
 
-from flask import request
-from typing import List, TypeVar
 from api.v1.auth.auth import Auth
-import re
-from base64 import b64decode
+from uuid import uuid4
 from models.user import User
 
 
 class SessionAuth(Auth):
     """class to define session authorization"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates session"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

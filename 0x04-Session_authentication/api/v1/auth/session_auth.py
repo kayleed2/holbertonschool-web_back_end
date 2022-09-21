c66e9ea0b5e79d444dea1bdc538fd59c8344cfa5
Kaylee Dao <kayleed2@gmail.com>
2022-09-21 10:07:02 -0500

    Getting current user

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,9 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Overload that returns a User based on cookie"""
+        _my_session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(_my_session_id)
+        return User.get(user_id)

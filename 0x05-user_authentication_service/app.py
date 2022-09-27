5d92cdca6206982ea48a51e5f9b8fcab1beef456
Kaylee Dao <kayleed2@gmail.com>
2022-09-27 15:54:03 -0700

    Getting profile

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -52,5 +52,16 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """Implements a profile function"""
+    inst = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(inst)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

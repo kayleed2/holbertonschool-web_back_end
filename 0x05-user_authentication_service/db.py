7967ffea37c315cbee1a9f289a046af134390b56
Kaylee Dao <kayleed2@gmail.com>
2022-09-27 10:03:14 -0700

    Try and except

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -36,3 +38,12 @@ class DB:
         self._session.add(new)
         self._session.commit()
         return new
+
+    def find_user_by(self, **kwargs) -> User:
+        """Gets user with keyword args"""
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError

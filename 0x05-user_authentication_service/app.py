6e35cd7a373074c7006e00ab8fdf687dab472829
Kaylee Dao <kayleed2@gmail.com>
2022-09-27 16:19:30 -0700

    update pwd

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
+"""Flask app is documented"""
 
-
-from re import A
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
@@ -81,8 +80,7 @@ def update_password():
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
     try:
-        token = AUTH.get_reset_password_token(email)
-        AUTH.update_password(reset_token, new_password)
+        token = AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)

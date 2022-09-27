f038a7e011c35d8f4867c3208f78200c1f8216bf
Kaylee Dao <kayleed2@gmail.com>
2022-09-27 10:44:49 -0700

    users()

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
-from crypt import methods
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def message():
     return jsonify(message='Bienvenue')
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+    try:
+        AUTH.register_user(email, pwd)
+        return jsonify({'email': email, 'message': 'user created'})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":

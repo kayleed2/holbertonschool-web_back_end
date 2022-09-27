47c777246533b3dc8c8c441438cc86a511e52d53
Kaylee Dao <kayleed2@gmail.com>
2022-09-27 10:55:46 -0700

    Valid login?

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -2,9 +2,10 @@
 """
 File that contains a hasing of a password method
 """
+
 from db import DB
 from base64 import encode
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -25,6 +26,14 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if login is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False
 
 
 def _hash_password(password: str) -> bytes:

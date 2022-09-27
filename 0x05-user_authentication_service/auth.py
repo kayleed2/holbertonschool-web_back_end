35f2033e66a9960efa28a29d139486436e4e4362
Kaylee Dao <kayleed2@gmail.com>
2022-09-27 15:39:47 -0700

    auth

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -52,9 +52,9 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user
         except NoResultFound:
             return None
+        return user
 
 
 def _hash_password(password: str) -> bytes:

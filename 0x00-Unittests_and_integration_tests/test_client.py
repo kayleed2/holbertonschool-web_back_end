7fb1fb34e2e73487cfd922c955ab812ab298842f
Kaylee Dao <kayleed2@gmail.com>
2022-10-03 15:46:47 -0500

    Task 6

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ from client import GithubOrgClient
 import unittest
 from parameterized import parameterized, parameterized_class
 from unittest import TestCase, mock
-from mock import patch
+from unittest.mock import patch
 
 
 class TestGithubOrgClient(TestCase):
@@ -19,13 +19,13 @@ class TestGithubOrgClient(TestCase):
     @patch("client.get_json",
            mock.MagicMock(return_value={'key': 'value'}))
     def test_org(self, org_name):
-        "test org method"
+        "test the org method"
         cli = GithubOrgClient(org_name)
         self.assertEqual(cli.org, {'key': 'value'})
 
     def test_public_repos_url(self):
         """Test public_repos_url method"""
-        with mock.patch("client.get_json",
+        with patch("client.get_json",
                    new_callable=mock.PropertyMock,
                    return_value={"repos_url": "url"}):
             cli = GithubOrgClient("google")

7c8d07f626984bfb7e955aeb3cf48ef21f9c58d5
Kaylee Dao <kayleed2@gmail.com>
2022-10-03 14:47:49 -0500

    Task 0

---
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module to test the utils.py file functions"""
+import unittest
+from unittest import TestCase
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test for access nested map function"""
+    @parameterized.expand([
+       ({"a": 1}, ("a",), 1),
+       ({"a": {"b": 2}}, ("a",), {"b": 2}),
+       ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expect):
+        """Test the access nested map"""
+        self.assertEqual(access_nested_map(nested_map, path), expect)

ab8c968ffdd4093106e22327e6e24f0c217d14a5
Kaylee Dao <kayleed2@gmail.com>
2022-10-06 13:56:05 -0500

    Task 0

--- 0x01-i18n/0-app.py
+++ 0x01-i18n/0-app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slahes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Simple message"""
     return render_template('0-index.html')

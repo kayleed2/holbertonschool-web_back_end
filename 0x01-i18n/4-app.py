223b863b9507bae76daa07a8da533a18baa5efb7
Kaylee Dao <kayleed2@gmail.com>
2022-10-06 14:19:46 -0500

    Task 4

--- 0x01-i18n/4-app.py
+++ 0x01-i18n/4-app.py
@@ -22,13 +22,16 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """Function to get locale selection"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Simple message"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":

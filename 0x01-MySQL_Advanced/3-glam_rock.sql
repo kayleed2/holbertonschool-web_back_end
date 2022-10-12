f38c386659da3f2f8fd1e674a3484136df281439
Kaylee Dao <kayleed2@gmail.com>
2022-10-12 10:01:47 -0500

    Task 3

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Scriopt that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (split - formed) as lifespan
 FROM metal_bands
-GROUP BY band_name
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
  

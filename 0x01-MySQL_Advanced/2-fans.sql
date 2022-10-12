44549261223f3f0dda40bf8be4a347218bc3c44a
Kaylee Dao <kayleed2@gmail.com>
2022-10-12 09:54:17 -0500

    Task 2

---
+++ 0x01-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- Scriopt that ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY NB_FANS DESC;
+ 

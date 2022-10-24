14e55c3928806fa948f7bbe5e551d81b78c9d36b
Kaylee Dao <kayleed2@gmail.com>
2022-10-24 11:35:28 -0500

    Fullserver repo

---
+++ 0x00-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,7 @@
+// function named readDatabase that accepts a file path as argument:
+
+import countStudents from '../3-read_file_async';
+
+const readDatabase = (path) => countStudents(path);
+
+module.exports = readDatabase;

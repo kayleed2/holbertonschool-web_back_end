4b0816fa9c812133e41b7c672c7f8f377756cc7a
Kaylee Dao <kayleed2@gmail.com>
2022-11-04 11:01:42 -0500

    Task 1

--- 0x02-queuing_system_in_js/1-redis_op.js
+++ 0x02-queuing_system_in_js/1-redis_op.js
@@ -22,3 +22,6 @@ const displaySchoolValue = (schoolName) => {
         console.log(value);
     });
 };
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');

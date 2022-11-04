ad84d1ee863d064757e00fc36fa691b3f748b0ee
Kaylee Dao <kayleed2@gmail.com>
2022-11-04 11:01:12 -0500

    Task 1

---
+++ 0x02-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,24 @@
+//Connect to redis server
+
+import { createClient } from 'redis';
+
+const client = createClient();
+
+    client.on('connect', () => {
+        console.log('Redis client connected to the server');
+    })
+    client.on('error', (err) => {
+        console.log('Redis client not connected to the server: ', err);
+    });
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, redis.print);
+};
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, value) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(value);
+    });
+};
